fec246995b3cca2f1513b9a8022f5c2cd99e74dd
MevsDaniel <danielmevs7@gmail.com>
2024-10-16 13:12:35 -0600

    coolpics

--- cool pics/coolpics.js
+++ cool pics/coolpics.js
@@ -11,13 +11,40 @@ function handleResize() {
     const menuButton = document.querySelector(".menu-button");
     
     if (window.innerWidth > 1000) {
-        menu.classList.remove("hide"); // Show the menu
-        menuButton.classList.add("hide"); // Hide the menu button
+        menu.classList.remove("hide"); 
+        menuButton.classList.add("hide"); 
     } else {
-        menu.classList.add("hide"); // Hide the menu
-        menuButton.classList.remove("hide"); // Show the menu button
+        menu.classList.add("hide"); 
+        menuButton.classList.remove("hide");
     }
 }
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+//viewer stuff
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+      <button class="close-viewer">X</button>
+      <img src="${pic}" alt="${alt}">
+      </div>`;
+  }
+
+
+function viewHandler(event) {
+    const clickedElement = event.target;
+    if (clickedElement.tagName === 'IMG') { 
+        const imgSrc = clickedElement.src.split('-sm').join('-full');
+        const altText = clickedElement.alt; 
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(imgSrc, altText));
+        document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+    }
+}
+
+function closeViewer() {
+    const viewer = document.querySelector('.viewer');
+    if (viewer) {
+        viewer.remove();
+    }
+}
+document.querySelector('.gallery').addEventListener('click', viewHandler);
